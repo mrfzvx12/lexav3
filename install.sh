0195ab2d92aa2e228e98df38fa0ae39b2e3dff83
Ma'ruf <72728486+mrfzvx12@users.noreply.github.com>
2021-05-08 23:56:08 +0700

    Update install.sh

--- install.sh
+++ install.sh
@@ -8,7 +8,6 @@ apt-get install tesseract
 wget -O ~/../usr/share/tessdata/ind.traineddata "https://github.com/tesseract-ocr/tessdata/blob/master/ind.traineddata?raw=true"
 
 npm i imgbb-uploader
-npm i got
 npm i
 
 echo "[*] All dependencies have been installed, please run the command \"npm start\" to immediately start the script"
