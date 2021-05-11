4efa7a8feeeaa158a7bf779bbdf4c60ecc96b7fb
Ma'ruf <72728486+mrfzvx12@users.noreply.github.com>
2021-05-11 11:49:34 +0700

    update index.js

--- index.js
+++ index.js
@@ -4,7 +4,7 @@ let path = require('path')
 let fs = require('fs')
 let package = JSON.parse(fs.readFileSync('./package.json'))
 const CFonts  = require('cfonts')
-const.say('LEXA', {
+cFonts.say('LEXA', {
   font: 'pallet',
   align: 'center',
   gradient: ['red', 'magenta']
